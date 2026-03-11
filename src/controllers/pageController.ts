d07d6a7bbc52d390446f1ce6ffcf352b5c7e59a5
manuoleoin <oleoin.prado@aluno.ifsp.edu.br>
2026-03-11 08:40:15 -0300

    site lançado com iniciação

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,24 @@
 import { Request, Response } from "express";
+import { title } from "node:process";
 
 export const home = (req: Request, res: Response) => {
-    res.send('home');
-    // res.render('pagess/page');
+    // res.send('home');
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os animais',
+            backgroud: 'allanimals.jpg'
+        }
+    });
 };
 
 export const dogs = (req: Request, res: Response) => {
-    res.send('dogs');
+    res.send('pages/page');
 };
 
 export const cats = (req: Request, res: Response) => {
-    res.send('cats');
+    res.send('pages/page');
 };
 
 export const fishes = (req: Request, res: Response) => {
-    res.send('fishes');
+    res.send('pages/page');
 };
